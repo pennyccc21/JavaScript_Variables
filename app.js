7cba607aeb2e1c0eabdc00dab00923b80b79a49a
Peter luque <Peterluque21@gmail.com>
2022-11-14 14:38:30 -0500

    finished exercise

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ console.log(
 );
 
 // Exercise 1
+const firstName = 'Peter';
+let lastName = 'Luque';
+var age = 23;
 
 // Exercise 2
+let fullName = firstName + ' ' + lastName;
+fullName = `${firstName} ${lastName}`;
 
+console.log(fullName);
 // Exercise 3
+
+let city = 'Silver Springs';
+let pastime = 'basketball';
+
+let myStory = `Hi, my name is ${fullName}. I live in ${city}, and I enjoy ${pastime} and coding.`;
+console.log(myStory);
